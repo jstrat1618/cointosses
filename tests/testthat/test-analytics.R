9827b386627b791e04dcd6e1885e10b775a56f4c
Justin <jstrat1.618@gmail.com>
2018-07-22 11:32:12 -0600

    Add tests for streaks.tossedCoin

--- tests/testthat/test-analytics.R
+++ tests/testthat/test-analytics.R
@@ -3,4 +3,11 @@ context("Testing Coin Analytics")
 test_that("Testing streaks", {
   x <- streaks(c("H", "H", "T", "H", "T", "T", "T"))
   expect_equal(x, c(2,3))
+
+  set.seed(2018)
+  x <- coin()
+  y <- toss(x, 50)
+  out <- streaks(y)
+
+  expect_equal(max(out$runs),6)
 })
