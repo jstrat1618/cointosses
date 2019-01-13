6ca5f4e9082097ad8ab8151f7da1b15e3c03c134
Justin <jstrat1.618@gmail.com>
2019-01-13 16:19:31 -0700

    Add test for tossing autocorrelated coin

--- tests/testthat/test-tosses.R
+++ tests/testthat/test-tosses.R
@@ -5,3 +5,12 @@ test_that("Testing the toss function", {
   should_be_H <- toss(prob = 1)
   expect_equal(should_be_H, "H")
 })
+
+test_that("Testing Autocorrelated Coins",{
+  x <-
+    autoCorrCoin(initial_prob = 1, p_given1 = 0,
+                    p_given0 = 1) %>%
+    toss(x, n=4)
+
+  expect_equal(x$trials, factor(c("H", "T", "H", "T"), levels = c("H", "T")))
+})
